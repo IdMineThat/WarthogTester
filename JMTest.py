232fbc7b04022a33f79dc82480296e40771f3a97
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2024-03-09 16:28:14 -0600

    Update JMTest.py

--- JMTest.py
+++ JMTest.py
@@ -1,7 +1,8 @@
 import subprocess
 
 output_file = "JM_Test.txt"
-IsThereOpenProcess = False
+Failure = False
+failureCounter = 0
 args = 'janusminer-windows -h us.acc-pool.pw -p 12000 -u af80d9a79c09dca66c7da9f7e6f5cfd6f48804d46c785eac --gpus="0,1" -t 36 -q 16'
 with open(output_file, "wb") as f:
     process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -9,8 +10,13 @@ with open(output_file, "wb") as f:
         print(line)
         f.write(line)
         if b"Thread#0: 0.000000 h/s" in line:
+            Failure = True
+        if Failure == True:
+            failureCounter +=1
+        if failureCounter == 20:
             process.terminate()
             break
+    
     for line in process.stderr:
         print(line)
         f.write(line)
