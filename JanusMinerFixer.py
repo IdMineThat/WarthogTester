762283f6b077f39ebec160e01ecaeb84febfee3c
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2024-03-11 07:38:39 -0500

    Update JanusMinerFixer.py

--- JanusMinerFixer.py
+++ JanusMinerFixer.py
@@ -1,6 +1,6 @@
 import subprocess
 
-args = 'janusminer-windows -h us.acc-pool.pw -p 12000 -u af80d9a79c09dca66c7da9f7e6f5cfd6f48804d46c785eac --gpus="0,1" -t 36 -q 16'
+args = 'janusminer-windows -h us.acc-pool.pw -p 12000 -u af80d9a79c09dca66c7da9f7e6f5cfd6f48804d46c785eac --gpus="0,1" -t 36 -q 8'
 
 while 1:
 
@@ -8,7 +8,7 @@ while 1:
 
     for line in process.stdout:
         print(line)
-        if b"Thread#0: 0.000000 h/s" in line:
+        if b": 0.000000 h/s" in line:
             process.terminate()
 
     for line in process.stderr:
